56b9ef873399324b3abf4eeddf460ae24bc04a18
undertherainysky <elgl9746@colorado.edu>
2026-03-12 18:36:37 +0000

    Refactored slightly, began planning

---
+++ sundowner_plotting/main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+
+# plan:
+    # first: consider what kinds of data we want
+        # idea 1: accumulation of precip
+            # pros: could generate cool graphs
+            # cons: requires so much data 
+        # idea 2: look at special days, like feb 13th or something
+            # pros: less data, unless if comparing across years
+            # cons: a little bit more creativity in graphing required
+        # idea 3: other accumulations
+            # best paired with idea 1, potentially multiple graphs across different variables
+            # pros: more even cooler graphs
+            # cons: a LOT of data
+    # second: import - done!
+    # third: 
