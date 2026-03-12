189ce5f0e19bed973f477344e058d10932d6bfb1
undertherainysky <elgl9746@colorado.edu>
2026-03-12 19:59:26 +0000

    Filled out init

--- sundowner_plotting/__init__.py
+++ sundowner_plotting/__init__.py
@@ -1,1 +1,8 @@
 # sundowner plotting init
+__version__ = "0.1.0"
+__author__ = "undertherainysky"
+__all__ = ["file_input", "main"]
+
+# imports
+from sundowner_plotting.file_input import read_file
+
