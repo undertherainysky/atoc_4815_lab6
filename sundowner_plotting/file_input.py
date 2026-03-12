bbaba45c198f61764f3ce163f9c437b96f1cda1d
undertherainysky <elgl9746@colorado.edu>
2026-03-12 19:53:18 +0000

    ABandoned previous method, switched to URL files

--- sundowner_plotting/file_input.py
+++ sundowner_plotting/file_input.py
@@ -1,16 +1,44 @@
 import pandas as pd
-from pathlib import Path
 
 
 # ATOC_4815/atoc_4815_lab6/data_files/wxobs20250213.txt
-# resource used: https://labex.io/pythoncheatsheet/cheatsheet/file-directory-path
+def read_file(file_name):
+    url = "https://sundowner.colorado.edu/weather/atoc1/" + file_name
 
+    df = pd.read_fwf(url, header=[0, 1], skiprows=[2])
 
+    date_col = [c for c in df.columns if c[1] == "Date"][0]
+    time_col = [c for c in df.columns if c[1] == "Time"][0]
 
+    t = (
+        df[time_col]
+        .astype(str)
+        .str.strip()
+        .str.replace(r"a$", "AM", regex=True)
+        .str.replace(r"p$", "PM", regex=True)
+    )
+
+    dt = pd.to_datetime(
+        df[date_col].astype(str).str.strip() + " " + t,
+        format="%m/%d/%y %I:%M%p",
+        errors="coerce",
+    )
+
+    df = df.set_index(dt).drop(columns=[date_col, time_col])
+    df.index.name = "datetime"
+
+    df.columns = [
+        "_".join([str(a).strip(), str(b).strip()]).replace(" ", "_").strip("_")
+        for a, b in df.columns
+    ]
+
+    return df
 
 
 
 if __name__ == "__main__":
-    # Test 1: Validate file path functionality
-    file_path_test = Path("..").joinpath("atoc_4815_lab6").joinpath("data_files").joinpath("wxobs20250213")
-    print(f"File path test: {file_path_test}")
+    # Test 1: 
+    test_df = read_file("wxobs20250213.txt")
+    print(f"{test_df}")
+
+    
